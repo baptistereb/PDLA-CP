35c3d7784cd9b4c113c8f3d397cb28370542e103
Baptiste <baptiste.rebillard@outlook.fr>
2024-10-07 16:44:59 +0200

    creating the SQL structure

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PDLA-CP
+
+## Run the project
+Initialize the SQL structure by using the "create_table.sql" script.
+
+## Structure SQL
+- users (<u>user_id</u>, pseudo, password, type {user, volunteer, moderator})
+- missions (<u>mission_id</u>, description, #user_id, mission_state {waiting, valid, realized}, motif, type {help, need_help})
+- connection (<u>connection_id</u>, #mission_id, #user_id, connection_type {help, need_help})
---
+++ create_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    pseudo VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_type ENUM('users', 'volunteer', 'moderator') NOT NULL
+);
+
+-- Création de la table mission
+CREATE TABLE IF NOT EXISTS missions (
+    mission_id INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    user_id INT,
+    mission_state ENUM('waiting', 'valid', 'realized') NOT NULL,
+    motif VARCHAR(255) DEFAULT '',
+    mission_type ENUM('help', 'need_help') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Création de la table connection
+CREATE TABLE IF NOT EXISTS connections (
+    connection_id INT AUTO_INCREMENT PRIMARY KEY,
+    mission_id INT,
+    user_id INT,
+    connection_type ENUM('help', 'need_help') NOT NULL,
+    FOREIGN KEY (mission_id) REFERENCES missions(mission_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
