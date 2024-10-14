219179c05370b8ff8bfcb64735e95aaf4465f804
Baptiste <baptiste.rebillard@outlook.fr>
2024-10-14 13:33:18 +0200

    fix: import the master view

--- src/main/java/org/pdla/Main.java
+++ src/main/java/org/pdla/Main.java
@@ -1,12 +1,8 @@
 package org.pdla;
-import org.pdla.models.UserManagement;
+import org.pdla.views.MasterView;
 
 public class Main {
     public static void main(String[] args) {
-      UserManagement userManagement = new UserManagement();
-        userManagement.CreateUser("test", "test","volunteer");
-
-
+        MasterView window = new MasterView();
     }
-
 }
---
+++ src/main/java/org/pdla/views/MasterView.java
@@ -0,0 +1,7 @@
+package org.pdla.views;
+
+public class MasterView {
+    public MasterView() {
+
+    }
+}
