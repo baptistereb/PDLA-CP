c54df2a2cca60e295955b53879f1804ca94535db
Leandro-rdz <leandro.rdz604@gmail.com>
2024-10-16 17:30:18 +0200

    feat :  create the main window

--- src/main/java/org/pdla/views/MasterView.java
+++ src/main/java/org/pdla/views/MasterView.java
@@ -1,7 +1,25 @@
 package org.pdla.views;
 
-public class MasterView {
-    public MasterView() {
+import javax.swing.*;
+import java.awt.*;
 
+public class MasterView {
+    private int width;
+    private int height;
+    private String title;
+
+    public MasterView( int width, int height, String title) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+        createAndShowGUI();
+    }
+
+    private void createAndShowGUI() {
+        JFrame frame = new JFrame(this.title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(this.width, this.height));
+        frame.pack();
+        frame.setVisible(true);
     }
 }
