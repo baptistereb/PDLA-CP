b011dcac04d8d307800e30a9ef05c944f521d04e
Baptiste <baptiste.rebillard@outlook.fr>
2024-10-16 17:47:12 +0200

    break : modify the views structure

--- src/main/java/org/pdla/views/MasterView.java
+++ src/main/java/org/pdla/views/MasterView.java
@@ -6,20 +6,25 @@ import java.awt.*;
 public class MasterView {
     private int width;
     private int height;
-    private String title;
+    private JFrame frame = new JFrame("Default");
 
     public MasterView( int width, int height, String title) {
         this.width = width;
         this.height = height;
-        this.title = title;
         createAndShowGUI();
+        setTitle(title);
+
+        SignupForm signupForm = new SignupForm(this.frame);
     }
 
     private void createAndShowGUI() {
-        JFrame frame = new JFrame(this.title);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(this.width, this.height));
-        frame.pack();
-        frame.setVisible(true);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setPreferredSize(new Dimension(this.width, this.height));
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+
+    private void setTitle(String name) {
+        this.frame.setTitle(name);
     }
 }
--- src/main/java/org/pdla/views/SignupForm.java
+++ src/main/java/org/pdla/views/SignupForm.java
@@ -1,4 +1,10 @@
 package org.pdla.views;
 
+import javax.swing.*;
+
 public class SignupForm {
+    private JFrame frame;
+    public SignupForm(JFrame frame) {
+        this.frame = frame;
+    }
 }
