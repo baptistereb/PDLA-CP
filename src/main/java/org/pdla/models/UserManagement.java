36887773b5c4ddb67cddb62ed7a2f205e987b77d
Baptiste <baptiste.rebillard@outlook.fr>
2024-11-06 13:27:24 +0100

    feat: login

--- src/main/java/org/pdla/models/UserManagement.java
+++ src/main/java/org/pdla/models/UserManagement.java
@@ -2,11 +2,11 @@ package org.pdla.models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class UserManagement {
-
-
     public void CreateUser(String username, String password, String user_type) {
         String insertSQL = "INSERT INTO users (pseudo, password, user_type) VALUES (?, ?, ?)";
         DatabaseConnection dbconn= new DatabaseConnection();
@@ -53,5 +53,28 @@ public class UserManagement {
             e.printStackTrace();
         }
         dbconn.closeConnection();
+    }
+
+    public boolean Login(String username, String password) {
+        String SQL = "SELECT password FROM users WHERE pseudo = ?";
+        DatabaseConnection dbconn= new DatabaseConnection();
+        try (Connection connection = dbconn.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
+
+            preparedStatement.setString(1, username);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getString("password").equals(password);
+                } else {
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error getting the password corresponding to a user in the database.");
+            e.printStackTrace();
+        }
+        dbconn.closeConnection();
+        return false;
     }
 }
--- src/main/java/org/pdla/views/LoginForm.java
+++ src/main/java/org/pdla/views/LoginForm.java
@@ -1,4 +1,64 @@
 package org.pdla.views;
+import org.pdla.controllers.LoginFormController;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class LoginForm {
+    private JFrame frame;
+
+    public LoginForm(JFrame frame) {
+        this.frame = frame;
+        createForm();
+    }
+
+    private void createForm() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel();
+        JTextField username = new JTextField();
+        username.setPreferredSize(new Dimension(200, 30));
+
+        JPasswordField password = new JPasswordField();
+        password.setPreferredSize(new Dimension(200, 30));
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setPreferredSize(new Dimension(200, 30));
+
+        JLabel logged_label = new JLabel("Vous êtes connecté");
+        JLabel failed_label = new JLabel("Identifiant ou mot de passe incorrecte");
+
+        panel.add(username);
+        panel.add(password);
+        panel.add(loginButton);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+
+        loginButton.addActionListener(e -> {
+            panel.remove(logged_label);
+            panel.remove(failed_label);
+
+            // actualiser la vue
+            panel.revalidate();
+            panel.repaint();
+            frame.setVisible(true);
+
+            LoginFormController loginFormController = new LoginFormController();
+            boolean loged = loginFormController.Login(
+                username.getText(),
+                new String(password.getPassword())
+            );
+
+            if(loged) {
+                panel.add(logged_label);
+                frame.setVisible(true);
+            } else {
+                panel.add(failed_label);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
