37058dc80d66bbfe0162f15e41f49182bb51046a
Leandro-rdz <leandro.rdz604@gmail.com>
2024-11-28 10:18:30 +0100

    Update LoginFormController.java

--- src/main/java/org/pdla/controllers/LoginFormController.java
+++ src/main/java/org/pdla/controllers/LoginFormController.java
@@ -10,6 +10,8 @@ public class LoginFormController {
     public boolean Login(String username, String password) {
         UserManagement userManagement = new UserManagement();
         //TODO Check if data is well formated
+        System.out.println("Login attempt for user: " + username);
+        System.out.println("Password: " + password);
         return userManagement.Login(username, password);
     }
 
