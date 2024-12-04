1b757e347688ab3c19ab379205956f67ffd1c489
Baptiste <baptiste.rebillard@outlook.fr>
2024-12-04 19:18:03 +0100

    fix : app actually run

--- src/main/java/org/pdla/models/Credentials.java
+++ src/main/java/org/pdla/models/Credentials.java
@@ -2,9 +2,9 @@ package org.pdla.models;
 
 public class Credentials {
     // Hardcoded credentials
-    private static String dbUrl;
-    private static String dbUser;
-    private static String dbPassword;
+    private static String dbUrl = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/projet_gei_033";
+    private static String dbUser = "projet_gei_033";
+    private static String dbPassword = "vo6Ahc4d";
     public String getUrl() {
         return dbUrl;
     }
