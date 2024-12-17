ccb476163ed6e56662c659966c03e6a1f0bdd32f
Baptiste <baptiste.rebillard@outlook.fr>
2024-12-17 09:59:45 +0100

    feat: adding a test for OfferHelpController

---
+++ src/test/java/org/pdla/controllers/OfferHelpFormControllerTest.java
@@ -0,0 +1,22 @@
+package org.pdla.controllers;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.pdla.models.Credentials;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AskHelpFormControllerTest {
+
+    @BeforeAll
+    static void setup()
+    {
+        Credentials.SetTestCredentials();
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendRequest() {
+        OfferHelpFormController offerHelpFormController = new OfferHelpFormController();
+        assertTrue(offerHelpFormController.sendRequest("description"));
+    }
+}
